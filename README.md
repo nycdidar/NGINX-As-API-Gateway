e96a01fef9f9500dc4660555112ae6bd474a89a1
Didarul Amin <didarul.amin@nbcuni.com>
2020-02-23 23:00:48 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# NGINX-As-API-Gateway
-NGINX As API Gateway
+# Setup
+```
+docker-compose up -d
+```
+
+Visit http://localhost
+
+
+http://localhost/api/
+
+
+http://test1.localhost/
+
+
+http://test2.localhost/
---
+++ nginx/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#set -e
+#backend="${BACKEND:-localhost}"
+#port="${PORT:-80}"
+#username="${USERNAME:-user}"
+#password="${PASSWORD:-pass}"
+
+#htpasswd -c -b /etc/nginx/.htpasswd "$username" "$password"
+
+#sed "s/BACKEND/$backend/; s/PORT/$port/" /etc/nginx/nginx.default.conf > /etc/nginx/nginx.conf
+#nginx -c /etc/nginx/nginx.conf
+
+#/usr/bin/htpasswd -c -b /etc/nginx/.htpasswd "test" "test"
+#service nginx restart
